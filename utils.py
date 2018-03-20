b7c4bfae00b1a19dd7f140816ee215664f186df2
ml9951 <ml9951@rit.edu>
2018-03-20 13:03:44 -0400

    Reuse the corner_form function in detect

--- utils.py
+++ utils.py
@@ -172,10 +172,7 @@ def decode(loc, priors, variances):
     boxes = torch.cat((
         priors[:, :2] + loc[:, :2] * variances[0] * priors[:, 2:],
         priors[:, 2:] * torch.exp(loc[:, 2:] * variances[1])), 1)
-    boxes[:, :2] -= boxes[:, 2:] / 2
-    boxes[:, 2:] += boxes[:, :2]
-    return boxes
-
+    return corner_form(boxes)
 
 def log_sum_exp(x):
     """Utility function for computing log_sum_exp while determining
