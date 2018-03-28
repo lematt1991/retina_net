2a09ac8c93bedcac112bf5c0fcdb3d01e71f77ec
ml9951 <ml9951@rit.edu>
2018-03-28 16:26:41 -0400

    Clean up focal loss

--- focal_loss.py
+++ focal_loss.py
@@ -37,12 +37,12 @@ class MultiBoxLoss(nn.Module):
         self.variance = variance
         self.alpha = Variable(torch.Tensor([0.25] + [0.75] * (num_classes - 1)), requires_grad=False)
 
-    def cross_entropy(self, conf_pred, target_labels, ious, num_classes):
+    def cross_entropy(self, conf_pred, target_labels, num_classes):
         num_pos = (target_labels > 0).sum().data[0]
         num_neg = max(128, num_pos * self.negpos_ratio)
 
         # hard negative mining
-        neg_conf = conf_pred[(ious < 0.3).unsqueeze(1).expand_as(conf_pred)].view(-1, num_classes)
+        neg_conf = conf_pred[(target_labels == 0).unsqueeze(1).expand_as(conf_pred)].view(-1, num_classes)
         _, idxs = F.softmax(neg_conf, dim=1)[:, 0].sort()
 
         mined_neg_conf = neg_conf[idxs[:num_neg]]
@@ -60,15 +60,16 @@ class MultiBoxLoss(nn.Module):
 
         return F.cross_entropy(conf, labels)
 
-    def focal_loss(self, conf_pred, target_labels, ious, num_classes):
-        onehot = Variable(torch.eye(num_classes)[target_labels.data])
+    def focal_loss(self, conf_pred, target_labels, num_classes):
+        td = target_labels.data
+        onehot = torch.eye(num_classes)[td[td >= 0]]
 
-        # Ignore ambiguous cases where overlap is close, but not close enough...
-        onehot[((ious > 0.3) & (ious < 0.5)).unsqueeze(1).expand_as(onehot)] = 0
+        onehot = Variable(onehot, requires_grad = False)
+
+        conf_pred = conf_pred[(td >= 0).unsqueeze(1).expand_as(conf_pred)].view(-1, num_classes)
 
         # Subtract max on each cell for numerical reasons
         # https://github.com/caffe2/caffe2/blob/master/modules/detectron/softmax_focal_loss_op.cu#L36-L41
-        conf_pred = conf_pred.view(-1, num_classes)
         conf_pred = conf_pred - conf_pred.max(dim=1)[0].unsqueeze(1).expand_as(conf_pred)
 
         pt = F.softmax(conf_pred, dim=1)
@@ -96,25 +97,22 @@ class MultiBoxLoss(nn.Module):
 
         target_boxes = targets[:, :, :4].contiguous().view(-1, 4)
         target_labels = targets[:, :, 4].contiguous().view(-1).long()
-        ious = targets[:, :, 5].contiguous().view(-1)
 
         conf_pred = conf_pred.view(-1, num_classes)
         loc_pred = loc_pred.view(-1, 4)
 
-        # conf_loss = self.cross_entropy(conf_pred, target_labels, ious, num_classes)
-        conf_loss = self.focal_loss(conf_pred, target_labels, ious, num_classes)
+        # conf_loss = self.cross_entropy(conf_pred, target_labels, num_classes)
+        conf_loss = self.focal_loss(conf_pred, target_labels, num_classes)
 
         # Location loss
-        mask = (ious >= 0.5).unsqueeze(1).expand_as(loc_pred)
+        mask = (target_labels > 0).unsqueeze(1).expand_as(loc_pred)
         pos_pred_boxes = loc_pred[mask].view(-1, 4)
         pos_target_boxes = target_boxes[mask].view(-1, 4)
+
         if len(pos_pred_boxes) > 0:
             loc_loss = F.smooth_l1_loss(pos_pred_boxes, pos_target_boxes)
         else:
             loc_loss = 0
-
-        num_pos = max((ious > 0.5).sum().data[0], 1)
-
 
         return loc_loss, conf_loss
 
